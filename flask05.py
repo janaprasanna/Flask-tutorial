6153a1271807eb3db23f967500ebd78cded5a9ab
jana prasanna <emailtojana27@gmail.com>
2021-07-11 15:48:19 +0530

    sessions in flask - creating ,deleting manually

--- flask05.py
+++ flask05.py
@@ -14,10 +14,12 @@ def login():
         session["uname"] = username
         return redirect(url_for("user"))
     else:
+        if "uname" in session:
+            return redirect(url_for("user"))
         return render_template('login.html')
+'''doubt on 17th & 18th statements. why we do that?'''
 
-
-
+''' from session dictionary, the "uname" keyword is accessed and is copied to 'un' variable'''
 @app.route('/user')
 def user():
     if "uname" in session:
@@ -25,6 +27,15 @@ def user():
         return f"<h4>Hello {un}! welcome to our website !</h4>"
     else:
         return redirect(url_for("login"))
+
+
+
+'''deleting an session manually'''
+'''similar syntax of deleting an element in a dictionary (using pop)'''
+@app.route('/logout')
+def logout():
+    session.pop("uname",None)
+    return redirect(url_for("login"))
 
 
 
