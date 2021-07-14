64a9750f74736b705f5af3b9e0ad0395fd143fd5
jana prasanna <emailtojana27@gmail.com>
2021-07-14 14:45:14 +0530

    sql alchemy - creating columns for the table

--- flask07.py
+++ flask07.py
@@ -1,10 +1,39 @@
 from flask import Flask, render_template, url_for, redirect, request, session, flash
 from datetime import timedelta
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+
 ''' tutorial on sql alchemy - saving datas permanently'''
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3'
+'''users.sqlite3 - 'users' represent the table name that is created to store information of users.'''
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+'''just to avoid some warnings, we are not tracking all the modifications to the db always hence is false'''
 app.permanent_session_lifetime = timedelta(minutes=5)
 app.secret_key = "jana"
+db = SQLAlchemy(app)
+'''database object to write queries'''
+
+
+
+
+class users(db.Model):
+    _id = db.column("Id",db.integer,primary_key=True)
+    name = db.column("Name",db.string(50))
+    email = db.column("Email",db.string(50))
+
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
+
+
+
+
+
+
+
+
+
 
 @app.route('/home')
 @app.route('/')
@@ -54,4 +83,5 @@ def logout():
 
 
 if __name__== "__main__":
+    db.create_all()
     app.run(debug=True)
